c108338cd8984d21c8b26ae2b78eac21814fa0f4
AdrienPoupa <adrien@poupa.fr>
2018-01-21 23:09:36 -0500

    Allow HTML in mails

--- resources/views/mail/classic_poll.blade.php
+++ resources/views/mail/classic_poll.blade.php
@@ -1,7 +1,8 @@
 @lang('mail.This is the message you have to send to the people you want to poll.\nNow, you have to send this message to everyone you want to poll.')
 <br/><br/>
-{{ $admin_name }} @lang('mail.has just created a poll called') {{ $title }}<br/>
-{{ sprintf(__('mail.Thanks for filling the poll at the link above') . ' :<br/><br/><a href="%1$s">%1$s</a>', $url) }}
+{{ $admin_name }} @lang('mail.has just created a poll called') {{ $title }}
+<br/><br/>
+{!! sprintf(__('mail.Thanks for filling the poll at the link above') . '<a href="%1$s">%1$s</a>', $url) !!}
 <br/><br/>
 @lang('mail.Thanks for your trust.')
 <br/>
--- resources/views/mail/classic_poll_admin.blade.php
+++ resources/views/mail/classic_poll_admin.blade.php
@@ -1,5 +1,6 @@
 @lang('mail.This message should NOT be sent to the polled people. It is private for the poll\'s creator.\n\nYou can now modify it at the link above')
-{{ sprintf(' :<br/><br/><a href="%1$s">%1$s</a>', $url) }}
+<br/><br/>
+{!! sprintf('<a href="%1$s">%1$s</a>', $url) !!}
 <br/><br/>
 @lang('mail.Thanks for your trust.')
 <br/>
